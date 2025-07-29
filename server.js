7a637a2638936e94b795f1518884b48aaf4e258f
ukonulucky <ukonulucky@gmail.com>
2025-07-29 01:48:17 +0100

    redis basics

--- server.js
+++ server.js
@@ -1,88 +1,85 @@
-
-const redis = require("redis")
+const redis = require("redis");
 
 const redisClient = redis.createClient({
-    host: "localhost",
-    port:6379
-})
+  host: "localhost",
+  port: 6379,
+});
 
-
-
-redisClient.on("error", (error) => { 
+redisClient.on("error", (error) => {
   console.error("Redis client error occured");
-})
+});
 
+async function connectRedisFunc() {
+  try {
+    // setting connection to redis
+    const res = await redisClient.connect();
+    console.log("redis server connected", res);
+    // storing key value pairs to redis
+    await redisClient.set("name", "lucky");
+    // retreiving store data
+    const data = await redisClient.get("name");
+    console.log(data);
 
-async function connectRedisFunc() { 
-    try {
-        // setting connection to redis
-     const res =   await redisClient.connect()
-        console.log("redis server connected", res);
-        // storing key value pairs to redis
-        await redisClient.set("name", "lucky")
-        // retreiving store data
-        const data =  await redisClient.get("name")
-        console.log(data);
+    // delete data
+    const deletedData = await redisClient.del("name");
 
-        // delete data
-        const deletedData = await redisClient.del("name")
+    await redisClient.set("count", 100);
 
-         await redisClient.set("count", 100)
-       
-        // increase count
-        await redisClient.incr("count")
-        // decrease count
+    // increase count
+    await redisClient.incr("count");
+    // decrease count
 
-         await redisClient.decr("count")
+    await redisClient.decr("count");
 
-        // decrease data by 
+    // decrease data by
 
-        // data structures ///
-        //  storing multiple data using string
-        await redisClient.mSet(["user:name", "Lucky", "user:age", "12", "user:country", "Nigeria"])
-        
-        const [name, age, country] = await redisClient.mGet(["user:name", "user:age", "user:country"])        
-        console.log(name, age, country)
-         await redisClient.decrBy("count",5)
-        const count = await redisClient.get("count")
-        console.log("count result", count)
-        
-        // list data
-        /* await redisClient.rPush("note", ["book1", "book2", "book3"])
- */
-        await redisClient.lPop("note")
-        await redisClient.rPush("note", ["new1", "new2"])
-        await redisClient.rPop("note")
-        const result4 = await redisClient.lRange("note", 0, -1)
-        console.log("result4", result4);
-        // sets data type
+    // data structures ///
+    //  storing multiple data using string
+    await redisClient.mSet([
+      "user:name",
+      "Lucky",
+      "user:age",
+      "12",
+      "user:country",
+      "Nigeria",
+    ]);
 
+    const [name, age, country] = await redisClient.mGet([
+      "user:name",
+      "user:age",
+      "user:country",
+    ]);
+    console.log(name, age, country);
+    await redisClient.decrBy("count", 5);
+    const count = await redisClient.get("count");
+    console.log("count result", count);
 
-        // used to add data to a set
-        await redisClient.sAdd("newSet", ["john", "friday"])
-        
-        // used to get members of a set
-        const setList = await redisClient.sMembers("newSet")
-        console.log(setList)
+    // list data
+    /* await redisClient.rPush("note", ["book1", "book2", "book3"])
+     */
+    await redisClient.lPop("note");
+    await redisClient.rPush("note", ["new1", "new2"]);
+    await redisClient.rPop("note");
+    const result4 = await redisClient.lRange("note", 0, -1);
+    console.log("result4", result4);
+    // sets data type
 
-        // check if a string is a member of a set
+    // used to add data to a set
+    await redisClient.sAdd("newSet", ["john", "friday"]);
 
-        const isMember = await redisClient.sIsMember("newSet","friday") 
-        console.log(isMember);
-        
+    // used to get members of a set
+    const setList = await redisClient.sMembers("newSet");
+    console.log(setList);
 
+    // check if a string is a member of a set
 
-       
-
-     
-        
-    } catch (error) {
-        console.log("Redis connection error:", error);
-         
-    } finally { 
-     await   redisClient.quit()
-    }
+    const isMember = await redisClient.sIsMember("newSet", "friday");
+    console.log(isMember);
+  } catch (error) {
+    console.log("Redis connection error:", error);
+  } finally {
+    await redisClient.quit();
+  }
 }
 
-
-connectRedisFunc()
+connectRedisFunc();
